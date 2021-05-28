f5e71fb4bb3204e2db59458442171f54a1c6ea96
yami-newbie <68190399+yami-newbie@users.noreply.github.com>
2021-05-28 15:44:56 +0700

    .

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/screen/home_screens.dart';
+
+import 'screen/Body.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: Colors.red),
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+    ),
+    home: HomeScreen(),
+    );
+  }
+
+}
