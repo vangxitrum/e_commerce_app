157b016070ec102d11d11cadc82c117e6a7ec638
yami-newbie <68190399+yami-newbie@users.noreply.github.com>
2021-06-12 18:24:12 +0700

    .

--- se346/lib/main.dart
+++ se346/lib/main.dart
@@ -1,21 +1,28 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:se346/Screens/AdminScreen/AdminMainScreen/overview_screen.dart';
-import 'package:se346/Screens/AdminScreen/OrderManagerMentScreen/order_mangaerment_screen.dart';
-import 'package:se346/Screens/AdminScreen/ProductManagementScreen/product_managerment_screen.dart';
-import 'package:se346/Screens/Login/login_screen.dart';
 import 'package:se346/Screens/Welcome/welcome_screen.dart';
 import 'package:se346/constants.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'Screens/AdminScreen/AdminMainScreen/overview_screen.dart';
+import 'Screens/AdminScreen/AdminMainScreen/overview_screen.dart';
+import 'Screens/AdminScreen/AdminMainScreen/overview_screen.dart';
+import 'Screens/AdminScreen/AdminMainScreen/overview_screen.dart';
+import 'Screens/AdminScreen/AdminMainScreen/overview_screen.dart';
+import 'Screens/AdminScreen/AdminMainScreen/overview_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
 }
+final FirebaseAuth auth = FirebaseAuth.instance;
 
 class MyApp extends StatelessWidget {
   final Future<FirebaseApp> fbApp = Firebase.initializeApp();
+  final CollectionReference adminList = FirebaseFirestore.instance.collection('admin');
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -35,7 +42,7 @@ class MyApp extends StatelessWidget {
           }
           else if(snaphost.hasData) {
             return StreamBuilder(
-              stream: FirebaseAuth.instance.authStateChanges(),
+              stream: auth.authStateChanges(),
               builder: (context, snapshot){
                 if(snapshot.connectionState == ConnectionState.active) {
                   Object? user = snapshot.data;
@@ -44,14 +51,30 @@ class MyApp extends StatelessWidget {
                     return WelcomeScreen();
                   }
                   else {
-
+                    return StreamBuilder(
+                      stream: adminList.snapshots(),
+                      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                        if(snapshot.hasData){
+                          print(snapshot.data!.docs.map((e) => Text(e['user'])));
+                          //return ListView(
+                            //children: snapshot.data!.docs.map((e) => ListTile(title: Text(e['user']),)).toList(),
+                          //);
+                        }
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      },);
                   }
                 }
 
-                return OverViewScreen();
+                return Scaffold(
+                  body: Center(
+                      child: Text("Loading...")
+                  ),
+                );
               },
             );
-              //WelcomeScreen();
+              //;
           }
           else {
             return Center(
